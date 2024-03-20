4f9ab345462dbd54c7f0f35df888b914a7d74c47
Yaneury Fermin <fermin.yaneury@gmail.com>
2024-03-20 15:12:11 -0700

    cleanup

---
+++ src-tauri/src/fetch.rs
@@ -0,0 +1,68 @@
+use {
+    serde::Serialize,
+    std::{
+        collections::HashMap, convert::From, fs, path::Path
+    },
+};
+
+#[derive(Serialize, Clone, Debug)]
+#[serde(rename_all="lowercase")]
+pub enum Category {
+    Picture,
+    Video,
+    Text,
+    Unknown
+}
+
+impl From<String> for Category {
+    fn from(extension: String) -> Category {
+        let category_extensions_map: HashMap<&str, Category> = [
+            ("png", Category::Picture),
+            ("jpg", Category::Picture),
+            ("gif", Category::Picture),
+            ("mp4", Category::Video),
+            ("mov", Category::Video),
+            ("txt", Category::Text)
+        ].into_iter().collect();
+
+        category_extensions_map.get(extension.as_str()).unwrap_or(&Category::Unknown).clone()
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct Entry {
+    filename: String,
+    category: Category,
+}
+
+#[tauri::command]
+pub async fn fetch<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Result<Vec<Entry>, String> {
+    let app_data_dir = app
+        .path_resolver()
+        .app_data_dir()
+        .ok_or("Failed to resolve $APPDATADIR")?;
+    let asset_dir = Path::new(&app_data_dir).join("assets");
+    let files =
+        fs::read_dir(asset_dir).map_err(|e| format!("Failed to open $APPDATADIR: {}", e))?;
+
+    files
+        .into_iter()
+        .map(|path_or| 
+            match path_or {
+                Ok(path) => {
+                    let path = path.path();
+                    let filename = path.file_name().unwrap().to_str().unwrap().to_owned();
+                    let extension = path.extension().unwrap().to_str().unwrap().to_owned();
+                    let category = Category::from(extension.clone());
+                    
+                    return Ok(Entry {
+                        filename: filename,
+                        category: category
+                    })
+                },
+                Err(err) => {
+                    return Err(format!("Failed to unwrap directory entry {}", err));
+                }
+        })
+        .collect()
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,44 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use {
-    serde::{Serialize},
-    std::{
-        fs,
-        path::Path
-    }
-};
-
-#[derive(Serialize, Debug)]
-enum Category {
-    Picture,
-    Video,
-    Text
-}
-
-#[derive(Serialize, Debug)]
-struct Entry {
-    filename: String,
-    category: Category
-}
-
-#[tauri::command]
-async fn fetch<R: tauri::Runtime>(app: tauri::AppHandle<R>) -> Vec<Entry> {
-    let app_data_dir = app.path_resolver().app_data_dir().expect("App data directory is empty");
-    let asset_dir = Path::new(&app_data_dir).join("assets");
-    let files = fs::read_dir(asset_dir).unwrap();
-    
-    files.into_iter().map(|p| {
-       Entry {
-            filename: p.unwrap().file_name().into_string().unwrap(),
-            category: Category::Picture
-        }
-    }).collect()
-}
+mod check;
+mod fetch;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![fetch])
+        .invoke_handler(tauri::generate_handler![fetch::fetch])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
