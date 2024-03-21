6d202a1a60ea6f44fee741eb630b7bf0027af468
Yaneury Fermin <fermin.yaneury@gmail.com>
2024-03-21 12:04:13 -0700

    Add orientation check for pics

--- src/Picture.tsx
+++ src/Picture.tsx
@@ -1,11 +1,48 @@
+import { useState, useEffect, useRef } from "react";
+
+import "./Picture.css";
+
+enum Orientation {
+  Pending,
+  Landscape,
+  Portrait
+}
+
 interface Props {
   url: string;
 }
 
 const Picture = ({ url }: Props) => {
+  const [orientation, setOrientation] = useState<Orientation>(Orientation.Pending);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    const img = imgRef.current;
+
+    if (img) {
+      img.onload = () => {
+        const isLandscape = img.naturalWidth > img.naturalHeight;
+        setOrientation(isLandscape ? Orientation.Landscape : Orientation.Portrait);
+      };
+
+      img.src = url;
+    }
+  }, [url]);
+
+  const getClass = (() => {
+    switch (orientation) {
+      case Orientation.Pending:
+        return "img-display-none"
+      case Orientation.Portrait:
+        return "img-display-portrait"
+      case Orientation.Landscape:
+        return "img-display-landscape"
+    }
+  });
+
   return (
-    <img src={url} />
-  )
+    <img ref={imgRef} src={url} className={getClass()} />
+  );
 }
 
 export default Picture;
