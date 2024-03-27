d51ae414c4e60f6e594b6203f4be49fca15311af
Yaneury Fermin <fermin.yaneury@gmail.com>
2024-03-27 10:51:17 -0700

    fix cycle bugs

--- src/Slideshow.tsx
+++ src/Slideshow.tsx
@@ -14,38 +14,29 @@ interface Props {
 
 const Slideshow = ({ memories, intervalInMs }: Props) => {
   const [position, setPosition] = useState(0);
-  const timerIdRef = useRef(0);
+  const timerIdRef = useRef<number | null>(null);
 
   const startTimer = () => {
+    if (timerIdRef.current !== null)
+      clearInterval(timerIdRef.current);
+
     timerIdRef.current = setInterval(() => {
-      console.log("Updating")
-      console.log(`Position: ${position}`)
-      console.log(`Plus: ${(position + 1) % memories.length}`)
       setPosition((position + 1) % memories.length);
     }, intervalInMs);
   }
 
-  const resetTimer = () => {
-    clearInterval(timerIdRef.current);
-    startTimer();
-  };
-
   useEffect(() => {
-    console.log("Effected")
     startTimer();
-
-    return () => clearInterval(timerIdRef.current);
+    return /*cleanup*/ () => {
+      if (timerIdRef.current !== null)
+        clearInterval(timerIdRef.current);
+    }
   }, [position]);
 
   const onChangeSlide = (forward: boolean) => {
-    clearInterval(timerIdRef.current);
-
     const size = memories.length;
     const newPosition = forward ? (position + 1) % size : (((position - 1) % size) + size) % size
-
     setPosition(newPosition);
-
-    resetTimer();
   }
 
   return (
